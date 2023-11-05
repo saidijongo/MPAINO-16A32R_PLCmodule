67e3c87b48f328ce4a8bd6a630ae82fbd75fb8be
Jongo Said (김정고) <31678025+saidijongo@users.noreply.github.com>
2023-11-05 22:10:17 +0900

    Create relayLEDs.ino

---
+++ relayLEDs.ino
@@ -0,0 +1,19 @@
+const int tatalLEDs = 32;
+//int ledPins[] = {5,6,7};
+int ledPins[] = { 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85};
+
+void setup(){
+  for(int pin=0; pin < tatalLEDs;pin++){
+    pinMode(ledPins[pin], OUTPUT);
+  }
+}
+
+void loop(){
+  for(int pin=0; pin < tatalLEDs;pin++){
+    digitalWrite(ledPins[pin], HIGH);
+    delay(300);
+    digitalWrite(ledPins[pin],LOW);
+  }
+
+}
+
